176e72767072d1c47cb6e49b1bb5aea6908d13be
AhmedKhattab24 <154933305+AhmedKhattab24@users.noreply.github.com>
2023-12-27 01:16:15 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ int num1;
+printf("enter first number:");
+scanf("%i",&num1);
+
+int num2;
+printf("enter second number:");
+scanf("%i",&num2);
+
+int num3;
+printf("enter third number:");
+scanf("%i",&num3);
+
+int result= (num1+num2+num3);
+printf("%i",result);
+    return 0;
+}
